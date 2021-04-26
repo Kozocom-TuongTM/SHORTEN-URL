b98cc7e3682e7e9b019a2f0fe58dd3a5bd9dc955
Tuong <tranminhtuong1210@gmail.com>
2021-04-26 21:16:22 +0700

    + add reponsive for mobile

--- ui/src/components/Shorten.jsx
+++ ui/src/components/Shorten.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Client from "../Client";
-
+import MediaQuery from 'react-responsive'
+ 
 class Shorten extends Component{
 
   constructor(props){
@@ -28,34 +29,57 @@ class Shorten extends Component{
 }
   render(){
 
-    const container = {
+    const container_isDeskop = {
       width: '100%',
       height: '130px',
       backgroundColor: '#0B1736'
+
+    }
+    const container_isMobile = {
+      width: '100%',
+      height: '200px',
+      backgroundColor: '#0B1736'
     }
    const container_input = {
-      width: '800px' ,
-      marginLeft: '120px',
+      width: '58.56%' ,
+      marginLeft: '8.9%',
       height: '60px',
     }
-    const container_button = {
+    const container_button_isDeskop = {
       marginTop: '-60px',
       marginRight:'116px',
       float: 'right',
-      width: '284px',
+      width: '21%',
       height: '60px'
     }
+    const container_button_isMobile = {
+      width: '21%',
+      height: '60px',
+      float: 'left',
+      marginTop: '10px',
+      marginLeft: '8.9%'
+    }
+
     
-  
   return(
-    <div style = {container}> 
-      <form>
+    <div> 
+      <MediaQuery minDeviceWidth={1156}> 
+      <form style = {container_isDeskop}>
             <label>Name: </label> 
-            <input style={container_input} type = "text" name ="long_url" className="form-control" value= {this.state.long_url} onChange={this.changeUrlNameHandler} placeholder="Shorten your link"/>       
-            <input  style={container_button} type = "submit" onClick={this.saveUrl} value = "Shorten" className="btn btn-primary"/>  
-           <div></div>
+            <input style={container_input} type = "text" name ="long_url" className="form-control" value= {this.state.long_url} onChange={this.changeUrlNameHandler} placeholder="Shorten your link"/>   
+            <input  style={container_button_isDeskop} type = "submit" onClick={this.saveUrl} value = "Shorten" className="btn btn-primary"/>    
       </form> 
+      </MediaQuery>
+
+      <MediaQuery maxDeviceWidth={1155}> 
+      <form style = {container_isMobile}>
+            <label>Name: </label> 
+            <input style={container_input} type = "text" name ="long_url" className="form-control" value= {this.state.long_url} onChange={this.changeUrlNameHandler} placeholder="Shorten your link"/>   
+            <input  style={container_button_isMobile} type = "submit" onClick={this.saveUrl} value = "Shorten" className="btn btn-primary"/>    
+      </form> 
+      </MediaQuery>
     </div>
+
     
    
   );
